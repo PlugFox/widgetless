bd55f57d9fbeeca6d80c4c413407bcb706bc3cf5
Plague Fox <plugfox@gmail.com>
2025-01-14 06:32:22 +0400

    Add frame rate limitter

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:io' as io;
 import 'dart:ui' as ui;
 
 import 'package:flutter/rendering.dart';
@@ -6,42 +7,58 @@ import 'package:l/l.dart';
 import 'package:widgetless/src/rendering/binding.dart';
 import 'package:widgetless/src/util/env.dart';
 
-void main() => runZonedGuarded<void>(() async {
-      final env = Env();
-      final shouldRender = env.get<bool>('render') == true;
-      final binding = Binding.instance
-        /* ..attachRootWidget(rootWidget) */
-        /* ..ensureVisualUpdate() */
-        ..deferFirstFrame();
-      final dispatcher = binding.platformDispatcher;
-      // Schedule and render a warm-up frame.
-      if (dispatcher.implicitView case ui.FlutterView view when shouldRender) {
-        binding
-          ..scheduleWarmUpFrame()
-          ..handleBeginFrame(Duration.zero)
-          ..handleDrawFrame();
-        render(view);
-      }
-      binding.allowFirstFrame();
-      final clock = Stopwatch()..start();
-      var frames = 0;
-      while (shouldRender && clock.elapsed.inSeconds < 15) {
-        // Render the funvas animation / frame in the render view.
-        binding
-          ..scheduleFrame()
-          ..handleBeginFrame(clock.elapsed)
-          ..handleDrawFrame();
-        for (final view in dispatcher.views) render(view);
-        //await Future<void>.delayed(const Duration(milliseconds: 16));
-        frames++;
-      }
-      final elapsed = clock.elapsed;
-      l
-        ..i('Rendered $frames frames in ${elapsed.inMilliseconds}ms')
-        ..i('Average FPS: ${frames ~/ elapsed.inSeconds} frames per second');
-    }, (error, stackTrace) {
-      l.e('Unhandled exception: $error\n$stackTrace');
-    });
+void main() => l.capture<void>(
+      () => runZonedGuarded<void>(
+        () async {
+          final env = Env();
+          final shouldRender = env.get<bool>('render') == true;
+          final binding = Binding.instance
+            /* ..attachRootWidget(rootWidget) */
+            /* ..ensureVisualUpdate() */
+            ..deferFirstFrame();
+          final dispatcher = binding.platformDispatcher;
+          // Schedule and render a warm-up frame.
+          if (dispatcher.implicitView case ui.FlutterView view when shouldRender) {
+            binding
+              ..scheduleWarmUpFrame()
+              ..handleBeginFrame(Duration.zero)
+              ..handleDrawFrame();
+            render(view);
+          }
+          binding.allowFirstFrame();
+          final clock = Stopwatch()..start();
+          var frames = 0;
+          while (shouldRender && clock.elapsed.inSeconds < 15) {
+            // Render the funvas animation / frame in the render view.
+            binding
+              ..scheduleFrame()
+              ..handleBeginFrame(clock.elapsed)
+              ..handleDrawFrame();
+            for (final view in dispatcher.views) render(view);
+            await Future<void>.delayed(const Duration(milliseconds: 16)); // Rate limit to 60 FPS.
+            frames++;
+          }
+          final elapsed = clock.elapsed;
+          l
+            ..i('Rendered $frames frames in ${elapsed.inMilliseconds}ms')
+            ..i('Average FPS: ${frames ~/ elapsed.inSeconds} frames per second');
+          io.exit(0);
+        },
+        l.e,
+      ),
+      const LogOptions(
+        handlePrint: true,
+        messageFormatting: _messageFormatting,
+        outputInRelease: true,
+        printColors: true,
+      ),
+    );
+
+/// Formats the log message.
+Object _messageFormatting(LogMessage log) => '${_timeFormat(log.timestamp)} | ${log.message}';
+
+/// Formats the time.
+String _timeFormat(DateTime time) => '${time.hour}:${time.minute.toString().padLeft(2, '0')}';
 
 void render(ui.FlutterView view) {
   // Получаем информацию о размерах экрана
