a8e1b64ae97ff78e710eef84042c458d64850335
Plague Fox <plugfox@gmail.com>
2025-01-16 02:14:36 +0400

    Update main

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'dart:async';
-import 'dart:ui' as ui;
 
-import 'package:flutter/rendering.dart';
 import 'package:l/l.dart';
 import 'package:widgetless/src/clock.dart';
 import 'package:widgetless/src/rendering/binding.dart';
@@ -9,56 +7,29 @@ import 'package:widgetless/src/rendering/binding.dart';
 void main() => l.capture<void>(
       () => runZonedGuarded<void>(
         () async {
-          final binding = Binding.instance..ensureVisualUpdate() /*  ..deferFirstFrame() */;
+          //final env = Env();
+          final binding = Binding.instance
+            ..deferFirstFrame()
+            //..ensureVisualUpdate()
+            ..scheduleWarmUpFrame()
+            ..handleBeginFrame(Duration.zero)
+            ..handleDrawFrame();
           final view = binding.platformDispatcher.views.first;
-          final clock = Clock(view: view);
-          /* binding
-            ..scheduleFrame()
-            ..allowFirstFrame(); */
+          final clock = Clock(view: view)..render();
+          binding.allowFirstFrame();
 
           final stopwatch = Stopwatch()..start();
-          Timer.periodic(const Duration(seconds: 1), (_) {
+
+          void onTick(Timer timer) {
             // Render the next frame for each available view.
             binding
               ..scheduleFrame()
               ..handleBeginFrame(stopwatch.elapsed)
               ..handleDrawFrame();
             clock.render();
-          });
+          }
 
-          /* final env = Env();
-          final shouldRender = env.get<bool>('render') == true;
-          final binding = Binding.instance
-            /* ..attachRootWidget(rootWidget) */
-            /* ..ensureVisualUpdate() */
-            ..deferFirstFrame();
-          final dispatcher = binding.platformDispatcher;
-          // Schedule and render a warm-up frame.
-          if (dispatcher.implicitView case ui.FlutterView view when shouldRender) {
-            binding
-              ..scheduleWarmUpFrame()
-              ..handleBeginFrame(Duration.zero)
-              ..handleDrawFrame();
-            render(view);
-          }
-          binding.allowFirstFrame();
-          final clock = Stopwatch()..start();
-          var frames = 0;
-          while (shouldRender && clock.elapsed.inSeconds < 15) {
-            // Render the next frame for each available view.
-            binding
-              ..scheduleFrame()
-              ..handleBeginFrame(clock.elapsed)
-              ..handleDrawFrame();
-            for (final view in dispatcher.views) render(view);
-            await Future<void>.delayed(const Duration(milliseconds: 16)); // Rate limit to 60 FPS.
-            frames++;
-          }
-          final elapsed = clock.elapsed;
-          l
-            ..i('Rendered $frames frames in ${elapsed.inMilliseconds}ms')
-            ..i('Average FPS: ${frames ~/ elapsed.inSeconds} frames per second');
-          io.exit(0); */
+          Timer.periodic(const Duration(seconds: 1), onTick);
         },
         (error, stackTrace) {
           l.e('An error occurred $error\n$stackTrace', stackTrace);
@@ -77,52 +48,3 @@ Object _messageFormatting(LogMessage log) => '${_timeFormat(log.timestamp)} | ${
 
 /// Formats the time.
 String _timeFormat(DateTime time) => '${time.hour}:${time.minute.toString().padLeft(2, '0')}';
-
-/// Paints the canvas.
-void paint(Canvas canvas, Size size) {
-  canvas
-    ..save()
-    ..clipRect(Offset.zero & size);
-  final paint = Paint()..style = PaintingStyle.fill;
-  canvas
-    ..drawPaint(paint..color = const Color(0xFFFFFFFF))
-    ..drawCircle(
-      size.center(Offset.zero),
-      size.shortestSide / 2 - 16,
-      paint..color = const Color(0xFF000000),
-    )
-    ..restore();
-}
-
-// TODO(plugfox): Pass the delta time and frame number to the paint method.
-// Mike Matiunin <plugfox@gmail.com>, 14 January 2025
-
-/// Renders the view.
-void render(ui.FlutterView view) {
-  // Получаем информацию о размерах экрана
-  final windowSize = view.physicalSize;
-  final pixelRatio = view.devicePixelRatio;
-  final logicalSize = windowSize / pixelRatio;
-  final rect = Rect.fromLTWH(0, 0, logicalSize.width, logicalSize.height);
-
-  // Создаем рекордер для записи команд рисования
-  final recorder = ui.PictureRecorder();
-  final canvas = Canvas(recorder);
-
-  // Рисуем на холсте
-  paint(canvas, windowSize);
-
-  // Создаем слой с картинкой
-  final pictureLayer = PictureLayer(rect)..picture = recorder.endRecording();
-  final rootLayer = OffsetLayer()..append(pictureLayer);
-
-  // Создаем сцену
-  final sceneBuilder = ui.SceneBuilder()
-    ..pushClipRect(rect) // Обрезаем всё за пределами доски
-    //..pushTransform(Float64List.fromList([1, 0, 0, 0, 1, 0, 0, 0, 1])) // Не трансформируем
-    ..pop();
-  final scene = rootLayer.buildScene(sceneBuilder);
-
-  // Отправляем сцену во вьюху
-  view.render(scene);
-}
